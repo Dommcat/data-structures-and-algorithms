5f71c46bcb88b4881c2f72bc1b19113b71a34b10
Dominick Martin <dominickbmartin@gmail.com>
2023-04-09 01:16:20 +0000

    CC15 All Tests Pass

---
+++ python/data_structures/binary_tree/README.md
@@ -0,0 +1,15 @@
+# Python 401: Code Challenge Class 15
+Binary Search Trees
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+No White Board for this challenge
+
+[//]: # (## Approach & Efficiency)
+
+[//]: # (<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->)
+
+## Solution Code
+<!-- Show how to run your code, and examples of it in action -->
+- [Code Challenge 15 - Binary Search Trees Code Example](trees.py)
+
--- python/data_structures/binary_tree/trees.py
+++ python/data_structures/binary_tree/trees.py
@@ -86,30 +86,6 @@ class BinaryTree:
 
 
 
-'''
-Create a Binary Search Tree class: This class should be a sub-class (or your languages equivalent) of the Binary Tree Class, with the following additional methods:
-
-Add
-Arguments: value
-Return: nothing
-
-Adds a new node with that value in the correct location in the binary search tree.
-Contains
-Argument: value
-
-Returns: boolean indicating whether or not the value is in the tree at least once.
-
-'''
-
-# class BinarySearchTree(BinaryTree):
-#   def __init__(self):
-#         self.root = None
-
-#     def add(self, value):
-#         pass
-
-#     def contains(self):
-#         pass
 
 
 
